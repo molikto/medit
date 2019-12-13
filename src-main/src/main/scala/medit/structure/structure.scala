546df3977a1b3a410e98137243e2d7777873f0c1
Minghao Liu <molikto@gmail.com>
2019-12-13 19:42:01 +0800

    Update structure.scala

--- src-main/src/main/scala/medit/structure/structure.scala
+++ src-main/src/main/scala/medit/structure/structure.scala
@@ -7,18 +7,29 @@ import upickle.default.{macroRW, ReadWriter => RW}
 import scala.collection.mutable
 
 sealed trait StructureException extends Exception
+
 object StructureException {
+
   case class UnfoldNotSupported() extends StructureException
+
   case class CannotUnfold() extends StructureException
+
   case class UnknownField() extends StructureException
-  case class FieldNotAllowedHere() extends StructureException
+
+  case class OnlySimple() extends StructureException
+
   case class DuplicateFieldInTemplate() extends StructureException
+
   case class DuplicateName() extends StructureException
+
   case class EmptyName() extends StructureException
+
   case class BlankConstants() extends StructureException
+
   case class UnknownTextStyle() extends StructureException
 
   case class UnknownReference() extends StructureException
+
 }
 
 
@@ -33,19 +44,23 @@ sealed trait TypeTag {
       n.index = types.indexWhere(_.name == name)
       if (n.index == -1) throw StructureException.UnknownReference()
   }
+
   /** MEDIT_EXTRA_END **/
 }
 
 object TypeTag {
+
   /** MEDIT_EXTRA_START **/
   sealed trait Coll extends TypeTag {
     def item: TypeTag
+
     def sizeLimit: Int = this match {
       case Opt(item) => 1
       case Arr(item) => Int.MaxValue
       case Bag(item) => Int.MaxValue
     }
   }
+
   /** MEDIT_EXTRA_END **/
   @upickle.implicits.key("str")
   case class Str(name: String) extends TypeTag {
@@ -54,22 +69,28 @@ object TypeTag {
       case _ => throw StructureException.UnknownTextStyle()
     }
   }
+
   @upickle.implicits.key("opt")
   case class Opt(item: TypeTag) extends Coll
+
   @upickle.implicits.key("arr")
   case class Arr(item: TypeTag) extends Coll
+
   @upickle.implicits.key("bag")
   case class Bag(item: TypeTag) extends Coll
+
   @upickle.implicits.key("ref")
   case class Ref(name: String) extends TypeTag {
     /** MEDIT_EXTRA_START **/
     var index = -1
     /** MEDIT_EXTRA_END **/
   }
+
   implicit val rw: RW[TypeTag] = RW.merge(macroRW[Str], macroRW[Opt], macroRW[Arr], macroRW[Bag], macroRW[Ref])
 }
 
 case class NameTypeTag(name: String, tag: TypeTag)
+
 object NameTypeTag {
   implicit val rw: RW[NameTypeTag] = macroRW
 }
@@ -77,9 +98,12 @@ object NameTypeTag {
 case class Case(name: String, fields: Seq[NameTypeTag], template: Option[Template] = None) {
   /** MEDIT_EXTRA_START **/
   val defaultTemplate = Template.nameDefault(name, fields)
+
   def templateOrDefault = template.getOrElse(defaultTemplate)
+
   /** MEDIT_EXTRA_END **/
 }
+
 object Case {
   implicit val rw: RW[Case] = macroRW
 }
@@ -95,6 +119,7 @@ sealed trait Type {
   }
 
   def name: String
+
   def check(types: Seq[Type]) = this match {
     case record: Type.Record =>
       if (record.fields.exists(_.name.isEmpty)) throw StructureException.EmptyName()
@@ -113,6 +138,7 @@ sealed trait Type {
         a.defaultTemplate.check(a.fields)
       })
   }
+
   /** MEDIT_EXTRA_END **/
 }
 
@@ -120,51 +146,60 @@ sealed trait Template {
   /** MEDIT_EXTRA_START **/
   def check(fields: Seq[NameTypeTag]): Unit = {
     val remaining = mutable.Set.from(fields.map(_.name))
-    def rec(t: Template, allowUnfold: Boolean = false, allowField: Boolean = true): Unit = {
+
+    def rec(t: Template, allowUnfold: Boolean = false, complex: Boolean = true): Unit = {
       t match {
-        case f: Template.FieldRef =>
-          // the editor depends on these, child - field relationship should be one - one
-          if (!allowField) throw StructureException.FieldNotAllowedHere()
-          f match {
-            case Template.Unfold(_) =>
-              if (!allowUnfold) throw StructureException.UnfoldNotSupported()
-            case _ =>
+        case s: Template.Simple =>
+          s match {
+            case Template.Keyword(str) =>
+              if (str.isEmpty) throw StructureException.BlankConstants()
+            case Template.Delimiter(str) =>
+              if (str.isEmpty) throw StructureException.BlankConstants()
+            case Template.Separator(str) =>
+              if (str.isEmpty) throw StructureException.BlankConstants()
+            case Template.Pad | Template.LeftPad | Template.RightPad =>
           }
-          f.index = fields.indexWhere(_.name == f.name)
-          if (f.index == -1) throw StructureException.UnknownField()
-          if (!remaining.contains(f.name)) throw StructureException.DuplicateFieldInTemplate()
-          remaining.remove(f.name)
-        case Template.Compose(content) =>
-          content.foreach(a => rec(a))
-        case Template.Tree(left, b1, content, sep, b2) =>
-          content match {
-            case Seq(f@Template.Unfold(_)) =>
-              rec(f, true)
-              fields(f.index).tag match {
-                case coll: TypeTag.Coll =>
+        case a =>
+          if (!complex) throw StructureException.OnlySimple()
+          a match {
+            case f: Template.FieldRef =>
+              // the editor depends on these, child - field relationship should be one - one
+              f match {
+                case Template.Unfold(_) =>
+                  if (!allowUnfold) throw StructureException.UnfoldNotSupported()
                 case _ =>
-                  throw StructureException.CannotUnfold()
               }
-            case _ =>
+              f.index = fields.indexWhere(_.name == f.name)
+              if (f.index == -1) throw StructureException.UnknownField()
+              if (!remaining.contains(f.name)) throw StructureException.DuplicateFieldInTemplate()
+              remaining.remove(f.name)
+            case Template.Compose(content) =>
               content.foreach(a => rec(a))
+            case Template.Tree(left, b1, content, sep, b2) =>
+              content match {
+                case Seq(f@Template.Unfold(_)) =>
+                  rec(f, true)
+                  fields(f.index).tag match {
+                    case coll: TypeTag.Coll =>
+                    case _ =>
+                      throw StructureException.CannotUnfold()
+                  }
+                case _ =>
+                  content.foreach(a => rec(a))
+              }
+              left.foreach(a => rec(a))
+              b1.foreach(a => rec(a, complex = false))
+              // because then the field will be repeated in node
+              sep.foreach(a => rec(a, complex = false))
+              b2.foreach(a => rec(a, complex = false))
           }
-          left.foreach(a => rec(a))
-          b1.foreach(a => rec(a))
-          // because then the field will be repeated in node
-          sep.foreach(a => rec(a, allowField = false))
-          b2.foreach(a => rec(a))
-        case Template.Keyword(str) =>
-          if (str.isEmpty) throw StructureException.BlankConstants()
-        case Template.Delimiter(str) =>
-          if (str.isEmpty) throw StructureException.BlankConstants()
-        case Template.Separator(str) =>
-          if (str.isEmpty) throw StructureException.BlankConstants()
-        case Template.Pad | Template.LeftPad | Template.RightPad =>
 
       }
     }
+
     rec(this)
   }
+
   /** MEDIT_EXTRA_END **/
 }
 
@@ -183,33 +218,46 @@ object Template {
       )
     }
   }
+
+  sealed trait Simple extends Template {
+  }
+
   sealed trait FieldRef extends Template {
     val name: String
     var index = -1
   }
+
   /** MEDIT_EXTRA_END **/
 
   @upickle.implicits.key("keyword")
-  case class Keyword(str: String) extends Template
+  case class Keyword(str: String) extends Simple
+
   @upickle.implicits.key("delimiter")
-  case class Delimiter(str: String) extends Template
+  case class Delimiter(str: String) extends Simple
+
   @upickle.implicits.key("separator")
-  case class Separator(str: String) extends Template
+  case class Separator(str: String) extends Simple
+
   // TODO pads and seperators has different insertion point behaviour, but this is fine? MPS is similar
   @upickle.implicits.key("pad")
-  case object Pad extends Template
+  case object Pad extends Simple
+
   @upickle.implicits.key("left_pad")
-  case object LeftPad extends Template
+  case object LeftPad extends Simple
+
   @upickle.implicits.key("right_pad")
-  case object RightPad extends Template
+  case object RightPad extends Simple
 
   @upickle.implicits.key("field")
   case class Field(name: String) extends FieldRef
+
   @upickle.implicits.key("unfold")
   case class Unfold(name: String) extends FieldRef
+
   //case class Opt(before: Seq[Template], name: String, after: Seq[Template]) extends FieldRef
   @upickle.implicits.key("tree")
   case class Tree(left: Seq[Template], b1: Seq[Template], content: Seq[Template], sep: Option[Template], b2: Seq[Template]) extends Template
+
   @upickle.implicits.key("compose")
   case class Compose(content: Seq[Template]) extends Template
 
@@ -219,13 +267,17 @@ object Template {
 }
 
 object Type {
+
   @upickle.implicits.key("record")
   case class Record(name: String, fields: Seq[NameTypeTag], template: Option[Template] = None) extends Type {
     val defaultTemplate = Template.nameDefault(name, fields)
+
     def templateOrDefault = template.getOrElse(defaultTemplate)
   }
+
   @upickle.implicits.key("sum")
   case class Sum(name: String, cases: Seq[Case]) extends Type
+
   implicit val rw: RW[Type] = RW.merge(macroRW[Record], macroRW[Sum])
 }
 
@@ -249,6 +301,7 @@ object Language {
     import upickle.default._
     write(language)
   }
+
   /** MEDIT_EXTRA_END **/
   implicit val rw: RW[Language] = macroRW
 }
